3c590436711c66fd3cbc76b522d6247ab17827b3
Chris Pulman <chris.pulman@yahoo.com>
2023-10-15 02:51:28 +0100

    Fix for #12

---
+++ src/TestSingleClient/Extensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+
+internal static class Extensions
+{
+    internal static TObject Dump<TObject>(this TObject @object)
+    {
+        var output = "NULL";
+        if (@object != null)
+        {
+            output = JsonSerializer.Serialize(@object, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+        }
+
+        Console.WriteLine($"[{@object?.GetType().Name}]:\r\n{output}");
+        return @object;
+    }
+}
