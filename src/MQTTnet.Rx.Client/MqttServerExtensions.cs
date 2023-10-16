6be8383b21668b3a07c9ded99a8e846385a3303f
Chris Pulman <chris.pulman@yahoo.com>
2023-10-16 01:27:48 +0100

    Add MqttServer Observables

---
+++ src/MQTTnet.Rx.Client/MqttServerExtensions.cs
@@ -0,0 +1,212 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using MQTTnet.Server;
+
+namespace MQTTnet.Rx.Client;
+
+/// <summary>
+/// MqttServerExtensions.
+/// </summary>
+public static class MqttServerExtensions
+{
+    /// <summary>
+    /// Applications the message not consumed.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of ApplicationMessageNotConsumedEventArgs.</returns>
+    public static IObservable<ApplicationMessageNotConsumedEventArgs> ApplicationMessageNotConsumed(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<ApplicationMessageNotConsumedEventArgs>(
+            handler => server.ApplicationMessageNotConsumedAsync += handler,
+            handler => server.ApplicationMessageNotConsumedAsync -= handler);
+
+    /// <summary>
+    /// Clients the acknowledged publish packet.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of ClientAcknowledgedPublishPacketEventArgs.</returns>
+    public static IObservable<ClientAcknowledgedPublishPacketEventArgs> ClientAcknowledgedPublishPacket(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<ClientAcknowledgedPublishPacketEventArgs>(
+            handler => server.ClientAcknowledgedPublishPacketAsync += handler,
+            handler => server.ClientAcknowledgedPublishPacketAsync -= handler);
+
+    /// <summary>
+    /// Client connected.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of ClientConnectedEventArgs.</returns>
+    public static IObservable<ClientConnectedEventArgs> ClientConnected(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<ClientConnectedEventArgs>(
+            handler => server.ClientConnectedAsync += handler,
+            handler => server.ClientConnectedAsync -= handler);
+
+    /// <summary>
+    /// Clients the disconnected.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of ClientDisconnectedEventArgs.</returns>
+    public static IObservable<ClientDisconnectedEventArgs> ClientDisconnected(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<ClientDisconnectedEventArgs>(
+            handler => server.ClientDisconnectedAsync += handler,
+            handler => server.ClientDisconnectedAsync -= handler);
+
+    /// <summary>
+    /// Clients the subscribed topic.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of ClientSubscribedTopicEventArgs.</returns>
+    public static IObservable<ClientSubscribedTopicEventArgs> ClientSubscribedTopic(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<ClientSubscribedTopicEventArgs>(
+            handler => server.ClientSubscribedTopicAsync += handler,
+            handler => server.ClientSubscribedTopicAsync -= handler);
+
+    /// <summary>
+    /// Clients the unsubscribed topic.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of ClientUnsubscribedTopicEventArgs.</returns>
+    public static IObservable<ClientUnsubscribedTopicEventArgs> ClientUnsubscribedTopic(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<ClientUnsubscribedTopicEventArgs>(
+            handler => server.ClientUnsubscribedTopicAsync += handler,
+            handler => server.ClientUnsubscribedTopicAsync -= handler);
+
+    /// <summary>
+    /// Interceptings the client enqueue.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of InterceptingClientApplicationMessageEnqueueEventArgs.</returns>
+    public static IObservable<InterceptingClientApplicationMessageEnqueueEventArgs> InterceptingClientEnqueue(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<InterceptingClientApplicationMessageEnqueueEventArgs>(
+            handler => server.InterceptingClientEnqueueAsync += handler,
+            handler => server.InterceptingClientEnqueueAsync -= handler);
+
+    /// <summary>
+    /// Interceptings the inbound packet.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of InterceptingPacketEventArgs.</returns>
+    public static IObservable<InterceptingPacketEventArgs> InterceptingInboundPacket(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<InterceptingPacketEventArgs>(
+            handler => server.InterceptingInboundPacketAsync += handler,
+            handler => server.InterceptingInboundPacketAsync -= handler);
+
+    /// <summary>
+    /// Interceptings the outbound packet.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of InterceptingPacketEventArgs.</returns>
+    public static IObservable<InterceptingPacketEventArgs> InterceptingOutboundPacket(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<InterceptingPacketEventArgs>(
+            handler => server.InterceptingOutboundPacketAsync += handler,
+            handler => server.InterceptingOutboundPacketAsync -= handler);
+
+    /// <summary>
+    /// Interceptings the publish.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of InterceptingPublishEventArgs.</returns>
+    public static IObservable<InterceptingPublishEventArgs> InterceptingPublish(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<InterceptingPublishEventArgs>(
+            handler => server.InterceptingPublishAsync += handler,
+            handler => server.InterceptingPublishAsync -= handler);
+
+    /// <summary>
+    /// Interceptings the subscription.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of InterceptingSubscriptionEventArgs.</returns>
+    public static IObservable<InterceptingSubscriptionEventArgs> InterceptingSubscription(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<InterceptingSubscriptionEventArgs>(
+            handler => server.InterceptingSubscriptionAsync += handler,
+            handler => server.InterceptingSubscriptionAsync -= handler);
+
+    /// <summary>
+    /// Interceptings the unsubscription.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of InterceptingUnsubscriptionEventArgs.</returns>
+    public static IObservable<InterceptingUnsubscriptionEventArgs> InterceptingUnsubscription(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<InterceptingUnsubscriptionEventArgs>(
+            handler => server.InterceptingUnsubscriptionAsync += handler,
+            handler => server.InterceptingUnsubscriptionAsync -= handler);
+
+    /// <summary>
+    /// Loadings the retained message.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of LoadingRetainedMessagesEventArgs.</returns>
+    public static IObservable<LoadingRetainedMessagesEventArgs> LoadingRetainedMessage(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<LoadingRetainedMessagesEventArgs>(
+            handler => server.LoadingRetainedMessageAsync += handler,
+            handler => server.LoadingRetainedMessageAsync -= handler);
+
+    /// <summary>
+    /// Preparings the session.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of EventArgs.</returns>
+    public static IObservable<EventArgs> PreparingSession(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<EventArgs>(
+            handler => server.PreparingSessionAsync += handler,
+            handler => server.PreparingSessionAsync -= handler);
+
+    /// <summary>
+    /// Retaineds the message changed.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of RetainedMessageChangedEventArgs.</returns>
+    public static IObservable<RetainedMessageChangedEventArgs> RetainedMessageChanged(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<RetainedMessageChangedEventArgs>(
+            handler => server.RetainedMessageChangedAsync += handler,
+            handler => server.RetainedMessageChangedAsync -= handler);
+
+    /// <summary>
+    /// Retaineds the messages cleared.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of EventArgs.</returns>
+    public static IObservable<EventArgs> RetainedMessagesCleared(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<EventArgs>(
+            handler => server.RetainedMessagesClearedAsync += handler,
+            handler => server.RetainedMessagesClearedAsync -= handler);
+
+    /// <summary>
+    /// Sessions the deleted.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of SessionDeletedEventArgs.</returns>
+    public static IObservable<SessionDeletedEventArgs> SessionDeleted(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<SessionDeletedEventArgs>(
+            handler => server.SessionDeletedAsync += handler,
+            handler => server.SessionDeletedAsync -= handler);
+
+    /// <summary>
+    /// Starteds the specified server.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of EventArgs.</returns>
+    public static IObservable<EventArgs> Started(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<EventArgs>(
+            handler => server.StartedAsync += handler,
+            handler => server.StartedAsync -= handler);
+
+    /// <summary>
+    /// Stoppeds the specified server.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of EventArgs.</returns>
+    public static IObservable<EventArgs> Stopped(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<EventArgs>(
+            handler => server.StoppedAsync += handler,
+            handler => server.StoppedAsync -= handler);
+
+    /// <summary>
+    /// Validatings the connection.
+    /// </summary>
+    /// <param name="server">The server.</param>
+    /// <returns>An Observable of ValidatingConnectionEventArgs.</returns>
+    public static IObservable<ValidatingConnectionEventArgs> ValidatingConnection(this MqttServer server) =>
+        CreateObservable.FromAsyncEvent<ValidatingConnectionEventArgs>(
+            handler => server.ValidatingConnectionAsync += handler,
+            handler => server.ValidatingConnectionAsync -= handler);
+}
--- src/TestSingleClient/Program.cs
+++ src/TestSingleClient/Program.cs
@@ -2,50 +2,47 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using MQTTnet;
 using MQTTnet.Rx.Client;
-using MQTTnet.Server;
 
+CompositeDisposable? disposable = default;
 var serverPort = 2883;
-var mqttServerOptions = new MqttServerOptionsBuilder()
-                        .WithDefaultEndpointPort(serverPort)
-                        .WithDefaultEndpoint().Build();
 
-var mqttFactory = new MqttFactory();
-var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions);
-mqttServer.ClientConnectedAsync += (args) =>
-{
-    Console.WriteLine($"SERVER: ClientConnectedAsync => clientId:{args.ClientId}");
-    return Task.CompletedTask;
-};
-mqttServer.ClientDisconnectedAsync += (args) =>
-{
-    Console.WriteLine($"SERVER: ClientDisconnectedAsync => clientId:{args.ClientId}");
-    return Task.CompletedTask;
-};
-Console.WriteLine("Starting MQTT server...");
-await mqttServer.StartAsync();
-Console.WriteLine("MQTT server is started.");
-Console.WriteLine("------------------------------------\n");
+var server =
+Create.MqttServer(builder => builder.WithDefaultEndpointPort(serverPort).WithDefaultEndpoint().Build())
+      .Subscribe(sub =>
+      {
+          disposable = sub.Disposable;
+          sub.Disposable.Add(sub.Server.ClientConnected().Subscribe(args => Console.WriteLine($"SERVER: ClientConnectedAsync => clientId:{args.ClientId}")));
+          sub.Disposable.Add(sub.Server.ClientDisconnected().Subscribe(args => Console.WriteLine($"SERVER: ClientDisconnectedAsync => clientId:{args.ClientId}")));
+      });
 
 var obsClient = Create.ManagedMqttClient()
                       .WithManagedClientOptions(options =>
                         options.WithClientOptions(c =>
                                     c.WithTcpServer("localhost", serverPort))
-                                ////.WithClientId("Client01"))
+                               ////.WithClientId("Client01"))
                                .WithAutoReconnectDelay(TimeSpan.FromSeconds(2)));
 
+var obsClient1 = Create.ManagedMqttClient()
+                      .WithManagedClientOptions(options =>
+                        options.WithClientOptions(c =>
+                                    c.WithTcpServer("localhost", serverPort))
+                               ////.WithClientId("Client01"))
+                               .WithAutoReconnectDelay(TimeSpan.FromSeconds(2)));
+disposable!.Add(
 obsClient.Subscribe(i =>
 {
-    i.Connected().Subscribe((_) =>
-        Console.WriteLine($"{DateTime.Now.Dump()}\t {_.ConnectResult.AssignedClientIdentifier} CLIENT: Connected with server."));
-    i.Disconnected().Subscribe((_) =>
-        Console.WriteLine($"{DateTime.Now.Dump()}\t CLIENT: Disconnected with server."));
-});
-var subClient = obsClient.Publish().RefCount();
-var s1 = subClient.SubscribeToTopic("FromMilliseconds")
+    disposable.Add(i.Connected().Subscribe((_) =>
+        Console.WriteLine($"{DateTime.Now.Dump()}\t CLIENT: Connected with server.")));
+    disposable.Add(i.Disconnected().Subscribe((_) =>
+        Console.WriteLine($"{DateTime.Now.Dump()}\t CLIENT: Disconnected with server.")));
+}));
+
+var s1 = obsClient.SubscribeToTopic("FromMilliseconds")
          .Subscribe(r => Console.WriteLine($"\tCLIENT S1: {r.ReasonCode} [{r.ApplicationMessage.Topic}] value : {r.ApplicationMessage.ConvertPayloadToString()}"));
 
 var s2 = obsClient.SubscribeToTopic("FromMilliseconds")
@@ -54,14 +51,13 @@ var s2 = obsClient.SubscribeToTopic("FromMilliseconds")
 var s3 = obsClient.SubscribeToTopic("FromMilliseconds")
          .Subscribe(r => Console.WriteLine($"\tCLIENT S3: {r.ReasonCode} [{r.ApplicationMessage.Topic}] value : {r.ApplicationMessage.ConvertPayloadToString()}"));
 
+var s4 = obsClient1.SubscribeToTopic("FromMilliseconds")
+         .Subscribe(r => Console.WriteLine($"\tCLIENT S4: {r.ReasonCode} [{r.ApplicationMessage.Topic}] value : {r.ApplicationMessage.ConvertPayloadToString()}"));
+
 Subject<(string topic, string payload)> message = new();
 
-Observable.Interval(TimeSpan.FromMilliseconds(1000)).Subscribe(i => message.OnNext(("FromMilliseconds", "{" + $"payload: {i}" + "}")));
-
-obsClient.PublishMessage(message).Subscribe(r =>
-{
-    // Console.WriteLine($"Publish result [{r.Exception == null}], Id:{r.ApplicationMessage.Id}, Topic:{r.ApplicationMessage.ApplicationMessage.Topic}");
-});
+disposable.Add(Observable.Interval(TimeSpan.FromMilliseconds(1000)).Subscribe(i => message.OnNext(("FromMilliseconds", "{" + $"payload: {i}" + "}"))));
+disposable.Add(obsClient.PublishMessage(message).Subscribe());
 
 await Task.Delay(3000);
 s2.Dispose();
@@ -69,4 +65,22 @@ Console.WriteLine("Dispose S2 ---------------");
 await Task.Delay(5000);
 s1.Dispose();
 Console.WriteLine("Dispose S1 ---------------");
+await Task.Delay(5000);
+s4.Dispose();
+Console.WriteLine("Dispose S4 ---------------");
+
 Console.Read();
+s3.Dispose();
+Console.WriteLine("Dispose S3 ---------------");
+
+Console.WriteLine("Press 'Escape' or 'Q' to exit.");
+
+while (Console.ReadKey(true).Key is ConsoleKey key && !(key == ConsoleKey.Escape || key == ConsoleKey.Q))
+{
+    Thread.Sleep(1);
+}
+
+Console.WriteLine("Disposing All ---------------");
+disposable.Dispose();
+Console.WriteLine("Disposed All ---------------");
+server.Dispose();
